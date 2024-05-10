14ae1354cb6703c84b5fdda3f555481fbd1e9fdf
martiindmv <martin.mitrov2@gmail.com>
2024-05-10 11:31:43 +0200

    Renaming the file to index.html since it needs to be read first. Also removing some attributes from PersonController.java since it was preventing me from running my application. Lastly, changing the HTML file.

--- src/main/java/com/example/InstagramFollowerCount/model/PersonController.java
+++ src/main/java/com/example/InstagramFollowerCount/model/PersonController.java
@@ -1,13 +1,9 @@
 package com.example.InstagramFollowerCount.model;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
 public class PersonController {
 
-    @GetMapping
     public String getPeople(Model model) {
         model.addAttribute("Mar", new Person("Alice"));
         return "people";
